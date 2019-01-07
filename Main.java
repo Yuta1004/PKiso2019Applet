2948e2349da411144712aadf06cf9865ad0d94c1
Nakagami Yuta <guguru0014@gmail.com>
2019-01-07 11:53:04 +0900

    [add]Mainクラスに基本的な描画処理を実装

--- Main.java
+++ Main.java
@@ -4,8 +4,24 @@ import java.awt.event.*;
 
 /* <applet code="Main.class" width=700 height=700></applet> */
 
-public class Main extends Applet{
+public class Main extends Applet implements Runnable{
+    Thread drawThread;
+
+    // スレッド初期化
+    public void start(){
+        drawThread = new Thread(this);
+        drawThread.start();
+    }
+
+    // 画面更新担当
+    public void run(){
+        try{
+            repaint();
+            Thread.sleep(10); 
+        }catch(InterruptedException e){}
+    }    
+    
     public void paint(Graphics g){
-        g.drawString("HelloWorld", 100, 100);
+    
     }
 }
