51f37e4439adf29c41d9ee3d0811671fb15792d2
Nakagami Yuta <guguru0014@gmail.com>
2019-01-07 12:02:14 +0900

    [add]Mainクラスにキーリスナを追加

--- Main.java
+++ Main.java
@@ -4,8 +4,13 @@ import java.awt.event.*;
 
 /* <applet code="Main.class" width=700 height=700></applet> */
 
-public class Main extends Applet implements Runnable{
+public class Main extends Applet implements Runnable, KeyListener{
     Thread drawThread;
+
+    // システム初期化
+    public void init(){
+        addKeyListener(this);
+    }
 
     // スレッド初期化
     public void start(){
@@ -25,4 +30,17 @@ public class Main extends Applet implements Runnable{
     public void paint(Graphics g){
     
     }
+
+    // キー入力(押)
+    public void keyPressed(KeyEvent e){
+     
+    }
+
+    // キー入力(離)
+    public void keyReleased(KeyEvent e){
+
+    }
+
+    // キー入力(使わない)
+    public void keyTyped(KeyEvent e){}
 }
