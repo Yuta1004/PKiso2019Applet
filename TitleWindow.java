66aae551a140848c584f06386f69fe1db73ad4fe
Nakagami Yuta <guguru0014@gmail.com>
2019-01-07 12:15:15 +0900

    [add]TitleWindowクラスをとりあえずで作った

---
+++ TitleWindow.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class TitleWindow implements Window{
+    public TitleWindow(){
+    }
+
+    public void draw(Graphics g){
+        g.drawString("Hello TitleWindow!!", 100, 100);
+    }
+
+    public void keyPressed(char key){
+    }
+
+    public void keyReleased(char key){
+    }
+}
