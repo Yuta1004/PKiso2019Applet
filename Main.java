5eeed327a00fd59bc2db43a1777319b477895f23
i17027 <i17027@imac135.local>
2019-01-07 11:21:54 +0900

    [add]Mainクラス仮実装

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/* <applet code="Main.class" width=700 height=700></applet> */
+
+public class Main extends Applet{
+    public void paint(Graphics g){
+        g.drawString("HelloWorld", 100, 100);
+    }
+}
