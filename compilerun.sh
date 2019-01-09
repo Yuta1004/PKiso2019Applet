d073cb74d29ed7b85cb404e700038fa4ae94eab7
Nakagami Yuta <guguru0014@gmail.com>
2019-01-09 10:44:59 +0900

    [change]compilerun.shでコンパイルエラーが起きた場合は実行をしないように

--- compilerun.sh
+++ compilerun.sh
@@ -1,4 +1,9 @@
 echo "Compiling..."
 javac Main.java
-echo "Running..."
-appletviewer Main.java
+
+if [ $1 -eq 0 ]; then
+    echo "Running..."
+    appletviewer Main.java
+else
+    echo "Compile Error"
+fi
