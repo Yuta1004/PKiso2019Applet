069f5eae06693256b6f9366c7d66a331fe98cffe
Nakagami Yuta <guguru0014@gmail.com>
2019-01-16 11:20:13 +0900

    [add]コメント追加

--- GameWindow.java
+++ GameWindow.java
@@ -6,20 +6,24 @@ public class GameWindow implements Window{
     private Main parentClass;
     private ArrayList<Note> notes = new ArrayList<Note>();
     private Random random;
-    
+
+    // コンストラクタ    
     public GameWindow(Main main){
         parentClass = main;
 
+        // ノーツ生成(デバッグ用)
         random = new Random();
         for(int i = 0; i < 100; i++){
            notes.add(new Note(random.nextInt(2), random.nextInt(1000)+300));
         }
     }
 
+    // 画面のイニシャライザ
     public void init(){
         System.out.println("Init GameWindow");
     }
 
+    // 描画
     public void draw(Graphics g){
         // 判定円
         g.drawOval(75, 200, 50, 50);
@@ -31,19 +35,23 @@ public class GameWindow implements Window{
         }
     }
 
+    // キーが押された時
     public void keyPressed(char key){
         Main.changeWindow("GameOver");
     }
 
+    // キーが離された時
     public void keyReleased(char key){
     }
 }
 
+// ノールクラス
 class Note{
     private int lane;
     private int yBias;
     private float offset;
 
+    // コンストラクタ
     public Note(int lane, float offset){
         this.lane = lane;
         this.offset = offset;
@@ -55,6 +63,7 @@ class Note{
         }
     }
 
+    // ノーツ描画
     public void draw(Graphics g){
         offset --;
         if(offset < -100 || 700 < offset) return;
