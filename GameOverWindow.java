0f05a6d1093c036f8fe9345aa7c4bf50abf2fd35
NakagamiYuta <guguru0014@gmail.com>
2019-01-26 17:27:29 +0900

    [add]ゲームオーバー画面でスコア・ランクが表示できるようにした

--- GameOverWindow.java
+++ GameOverWindow.java
@@ -2,10 +2,30 @@ import java.awt.*;
 
 public class GameOverWindow implements Window{
     private Main parentClass;
+    public static float score = 0;
 
+    private String scoreStr;
+    private Image bgImg, percentImg, rankImg, letterGameOver[] = new Image[100];
 
     public GameOverWindow(Main main){
         parentClass = main;
+
+        // 画像読み込み(スコア表示用フォント)
+        scoreStr = String.format("%.2f", score).replace(".", "@");
+        for(int idx = 0; idx < scoreStr.length(); idx++){
+            letterGameOver[idx] = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/" + scoreStr.charAt(idx) + ".png");
+        }
+        letterGameOver[scoreStr.length()] = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/p.png");
+
+        // 画像読み込み(ランク)
+        if(score > 95.0f) rankImg = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/S.png");
+        else if(score > 90.0f) rankImg = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/A.png");
+        else if(score > 80.0f) rankImg = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/B.png");
+        else if(score > 70.0f) rankImg = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/C.png");
+        else rankImg = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/D.png");
+
+        // 画像読み込み(背景とか)
+        bgImg = parentClass.getImage(parentClass.getCodeBase(), "./res/bg_garden.png");
     }
 
     public void init(){
@@ -13,7 +33,16 @@ public class GameOverWindow implements Window{
     }
 
     public void draw(Graphics g){
-        g.drawString("Hello GameOverWindow!!", 100, 100);
+        g.drawImage(bgImg, 0, 0, 700, 700, parentClass);
+
+        // GameOverの文字
+        int space = scoreStr.length() * 80 / 2;
+        for(int idx = 0; idx < scoreStr.length() + 1; idx++){
+            g.drawImage(letterGameOver[idx], idx * 80 + space, 200, 80, 80, parentClass);
+        }
+
+        // ランク
+        g.drawImage(rankImg, 300, 350, 100, 100, parentClass);
     }
 
     public void keyPressed(char key){
