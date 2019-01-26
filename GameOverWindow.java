17399e3a02927d8752119eb95e5b5db9faf2f1ff
NakagamiYuta <guguru0014@gmail.com>
2019-01-26 17:58:53 +0900

    [change]ゲームオーバー画面のデザインを少し変えた

--- GameOverWindow.java
+++ GameOverWindow.java
@@ -5,10 +5,18 @@ public class GameOverWindow implements Window{
     public static float score = 0;
 
     private String scoreStr;
-    private Image bgImg, percentImg, rankImg, letterGameOver[] = new Image[100];
+    private Image bgImg, scoreboardImg, percentImg, rankImg, letterGameOver[] = new Image[100];
 
     public GameOverWindow(Main main){
         parentClass = main;
+
+        // 画像読み込み(背景とか)
+        scoreboardImg = parentClass.getImage(parentClass.getCodeBase(), "./res/score_board.png");
+        bgImg = parentClass.getImage(parentClass.getCodeBase(), "./res/bg_sky.jpg");
+    }
+
+    public void init(){
+        System.out.println("Init GameOverWindow");
 
         // 画像読み込み(スコア表示用フォント)
         scoreStr = String.format("%.2f", score).replace(".", "@");
@@ -24,25 +32,20 @@ public class GameOverWindow implements Window{
         else if(score > 70.0f) rankImg = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/C.png");
         else rankImg = parentClass.getImage(parentClass.getCodeBase(), "./res/letter/D.png");
 
-        // 画像読み込み(背景とか)
-        bgImg = parentClass.getImage(parentClass.getCodeBase(), "./res/bg_garden.png");
-    }
-
-    public void init(){
-        System.out.println("Init GameOverWindow");
     }
 
     public void draw(Graphics g){
         g.drawImage(bgImg, 0, 0, 700, 700, parentClass);
+        g.drawImage(scoreboardImg, 80, 300, 540, 300, parentClass);
 
         // GameOverの文字
-        int space = scoreStr.length() * 80 / 2;
+        int space = (scoreStr.length() + 1) * 80 / 2;
         for(int idx = 0; idx < scoreStr.length() + 1; idx++){
-            g.drawImage(letterGameOver[idx], idx * 80 + space, 200, 80, 80, parentClass);
+            g.drawImage(letterGameOver[idx], idx * 80 - space + 350, 350, 80, 80, parentClass);
         }
 
         // ランク
-        g.drawImage(rankImg, 300, 350, 100, 100, parentClass);
+        g.drawImage(rankImg, 300, 460, 100, 100, parentClass);
     }
 
     public void keyPressed(char key){
